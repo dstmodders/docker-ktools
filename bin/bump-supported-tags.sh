db62eeabece468068b93b89867d80dcf1d5e0fc5
Victor Popkov <victor@popkov.me>
2026-09-20 23:09:42 +0300

    Bump ImageMagick from 7.1.2-10 to 7.1.2-11

--- bin/bump-supported-tags.sh
+++ bin/bump-supported-tags.sh
@@ -70,10 +70,10 @@ print_url() {
 }
 
 # reference:
-#   4.5.1-imagemagick-7.1.2-10-alpine, 4.5.1-alpine, 4.5.1, alpine, latest
-#   4.5.1-imagemagick-7.1.2-10-debian, 4.5.1-debian, debian
-#   4.5.0-imagemagick-7.1.2-10-alpine, 4.5.0-alpine, 4.5.0
-#   4.5.0-imagemagick-7.1.2-10-debian, 4.5.0-debian
+#   4.5.1-imagemagick-7.1.2-11-alpine, 4.5.1-alpine, 4.5.1, alpine, latest
+#   4.5.1-imagemagick-7.1.2-11-debian, 4.5.1-debian, debian
+#   4.5.0-imagemagick-7.1.2-11-alpine, 4.5.0-alpine, 4.5.0
+#   4.5.0-imagemagick-7.1.2-11-debian, 4.5.0-debian
 print_latest_tags() {
   for key in "${LATEST_VERSIONS_KEYS[@]}"; do
     for dist in "${DISTS[@]}"; do
